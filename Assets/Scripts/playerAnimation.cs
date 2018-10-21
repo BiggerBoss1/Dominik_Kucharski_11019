e682805fb6ed829a4274e48a96a78d68ac9bb17b
BiggerBoss1 <44347985+BiggerBoss1@users.noreply.github.com>
2018-10-21 23:14:46 +0200

    Zadanie

---
+++ Assets/Scripts/playerAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimation : MonoBehaviour {
+
+    public Animator anim;
+	void Start () {
+
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.Play("Run", -1);
+        }
+		
+	}
+}
